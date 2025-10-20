daf28a6a324d5c8fcd5a1a6d14d85e87e00b9053
jakub.dunak <jakub.dunak@gmail.com>
2025-10-20 22:24:53 +0200

    refactor: remove email login option from Amplify configuration in App.js to simplify user authentication setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,9 +17,6 @@ Amplify.configure({
     Cognito: {
       userPoolId: process.env.REACT_APP_USER_POOL_ID || '',
       userPoolClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID || '',
-      loginWith: {
-        email: true,
-      }
     }
   },
   API: {
